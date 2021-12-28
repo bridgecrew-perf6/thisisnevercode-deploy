5758700f71a20cb472f59540a82ebfbfd1a1b66f
hwibaski <hwibaski@gmail.com>
2021-12-29 06:21:23 +0900

    Test

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,4 +1,4 @@
-const host = 'http://ec2-3-35-133-167.ap-northeast-2.compute.amazonaws.com' || 'localhost';
+const host = 'ec2-3-35-133-167.ap-northeast-2.compute.amazonaws.com' || 'localhost';
 const port = 8000;
 
 const API_ENDPOINT = `http://${host}:${port}`;
