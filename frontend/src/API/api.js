a7b126e15e4cb5fae297e7e08b2a432aabe76d06
hwibaski <hwibaski@gmail.com>
2021-12-29 06:20:10 +0900

    Test

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,4 +1,4 @@
-const host = process.env.REACT_APP_API_ENDPOINT || 'localhost';
+const host = 'http://ec2-3-35-133-167.ap-northeast-2.compute.amazonaws.com' || 'localhost';
 const port = 8000;
 
 const API_ENDPOINT = `http://${host}:${port}`;
